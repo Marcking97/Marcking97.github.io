d08def4dc71c1baac0f8bd74bb27d309d4ad2801
Marco <magc.guillen76@gmail.com>
2025-05-18 11:44:45 -0600

    Change js and index

--- resources/js/main.js
+++ resources/js/main.js
@@ -35,16 +35,13 @@ function typeWriter(text, elementId, delay = 30) {
   typing();
 }
 
-const screen1 = document.getElementsById("screen1");
-if (screen1.classList.contains("active")) {
-  let currentIndex = 0;
-  const slides = document.querySelectorAll(".slide");
+let currentIndex = 0;
+const slides = document.querySelectorAll(".slide");
 
-  function showNextSlide() {
-    slides[currentIndex].classList.remove("active");
-    currentIndex = (currentIndex + 1) % slides.length;
-    slides[currentIndex].classList.add("active");
-  }
+function showNextSlide() {
+  slides[currentIndex].classList.remove("active");
+  currentIndex = (currentIndex + 1) % slides.length;
+  slides[currentIndex].classList.add("active");
 }
 
 setInterval(showNextSlide, 4000);
