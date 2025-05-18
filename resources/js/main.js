5332670e29e4babd2c6f5b7e90daff4b7d0d54cd
Marco <magc.guillen76@gmail.com>
2025-05-18 11:39:47 -0600

    Change js

--- resources/js/main.js
+++ resources/js/main.js
@@ -35,13 +35,16 @@ function typeWriter(text, elementId, delay = 30) {
   typing();
 }
 
-let currentIndex = 0;
-const slides = document.querySelectorAll(".slide");
+const screen1 = document.getElementsById("screen1");
+if (screen1.classList.contains("active")) {
+  let currentIndex = 0;
+  const slides = document.querySelectorAll(".slide");
 
-function showNextSlide() {
-  slides[currentIndex].classList.remove("active");
-  currentIndex = (currentIndex + 1) % slides.length;
-  slides[currentIndex].classList.add("active");
+  function showNextSlide() {
+    slides[currentIndex].classList.remove("active");
+    currentIndex = (currentIndex + 1) % slides.length;
+    slides[currentIndex].classList.add("active");
+  }
 }
 
 setInterval(showNextSlide, 4000);
